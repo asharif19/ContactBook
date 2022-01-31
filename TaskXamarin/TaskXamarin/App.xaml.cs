ea296f83b0679418e4404c292daeadb789b43420
asharif19 <37584664+asharif19@users.noreply.github.com>
2022-01-31 12:47:41 +0200

    Add project files.

---
+++ TaskXamarin/TaskXamarin/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TaskXamarin.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TaskXamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new ContactsPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
